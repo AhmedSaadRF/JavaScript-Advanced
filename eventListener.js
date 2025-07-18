febe6a89796508568e4d69a8baa9903fa241c1fa
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-07-18 21:18:37 +0300

    Upload Project

--- eventListener.js
+++ eventListener.js
@@ -1,6 +1,14 @@
 document.getElementById("calc-btn").addEventListener("click", function () {
   const firstNumber = document.getElementById("firstNumber").value;
   const secondNumber = document.getElementById("secondNumber").value;
-  const result = divideNumbers(firstNumber, secondNumber);
-  document.getElementById("result").innerHTML = result;
+
+  try {
+    const result = divideNumbers(firstNumber, secondNumber);
+  } catch (error) {
+    alert("Error: " + error);
+  }
+
+  if (result != "unknown") {
+    document.getElementById("result").innerHTML = result;
+  }
 });
--- logic.js
+++ logic.js
@@ -1,3 +1,6 @@
 function divideNumbers(num1, num2) {
+  if (num2 == 0) {
+    throw "Division by zero is not allowed.";
+  }
   return num1 / num2;
 }
